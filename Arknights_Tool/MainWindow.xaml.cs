f7fd34717ef554afe41f35d22234e0e6368998c7
KINOVE <1437818923@qq.com>
2022-02-15 16:39:24 +0800

    对计算后的理智和时间显示进行了优化

--- Arknights_Tool/MainWindow.xaml.cs
+++ Arknights_Tool/MainWindow.xaml.cs
@@ -41,28 +41,44 @@ namespace Arknights_Tool
         //声明计时器
         private DispatcherTimer showtimer;
 
-        public void ShowCurLz(object sender, EventArgs e)
+        public void ShowCurLz(object sender, EventArgs e)   //显示当前理智
         {
             Cal_Lz();
         }
 
-        public void Cal_Lz()
+        public void Cal_Lz()    //计算剩余理智
         {
 
             lizhi_now.Text = Convert.ToString(Info.info.lz_start + Math.Floor((double)((DateTime.Now - Info.info.time_start).TotalMinutes / 6)));
             lizhi_full.Text = Convert.ToString(Info.info.lz_full);
             TimeSpan ts1 = Info.info.time_full.Subtract(DateTime.Now);
-            if (ts1.Days == 0)
+            if(ts1 < TimeSpan.Zero)//如果已经回满了
             {
-                last_time.Text = ts1.Hours.ToString() + " 小时 "
-                            + ts1.Minutes.ToString() + " 分钟 ";
+                lizhi_now.Text = lizhi_full.Text; //限定为最大值
+                caution_img.Visibility = Visibility.Visible; //打开警告图标显示
+                caution.Visibility = Visibility.Visible;    //打开警告文字显示
+                last_time.Visibility = Visibility.Collapsed;    //关闭正常文字显示
+                caution_text.Text = "已于";
             }
-            else
+            else //如果没有回满
             {
-                last_time.Text = " " + ts1.Days.ToString() + " 天 "
-                            + ts1.Hours.ToString() + " 小时 "
-                            + ts1.Minutes.ToString() + " 分钟 ";
+                caution_img.Visibility = Visibility.Collapsed; //关闭警告图标显示
+                caution.Visibility = Visibility.Collapsed;    //关闭警告文字显示
+                last_time.Visibility = Visibility.Visible;    //打开正常文字显示
+                caution_text.Text = "将于";
+                if (ts1.Days == 0)
+                {
+                    last_time.Text = ts1.Hours.ToString() + " 小时 "
+                                + ts1.Minutes.ToString() + " 分钟 ";
+                }
+                else
+                {
+                    last_time.Text = " " + ts1.Days.ToString() + " 天 "
+                                + ts1.Hours.ToString() + " 小时 "
+                                + ts1.Minutes.ToString() + " 分钟 ";
+                }
             }
+            
             if (Info.info.time_full.Day == DateTime.Now.Day)
             {
                 time_test.Text = (Info.info.time_full).ToString(" HH:mm ");
@@ -148,21 +164,20 @@ namespace Arknights_Tool
             //test
         }//负责存储【记录时间】【回满时间】【初始理智】【理智上限】
 
-        public void Write()
+        public void Write() //写入文件
         {
-            //返回缩进的 Json 字符串
-            string output = JsonConvert.SerializeObject(Info.info, Formatting.Indented);
+            string output = JsonConvert.SerializeObject(Info.info, Formatting.Indented);    //返回缩进的 Json 字符串
             File.WriteAllText("info.json", output); //输出json内容到info.json
 
         }
 
-        private void Window_Closed(object sender, EventArgs e)
+        private void Window_Closed(object sender, EventArgs e)  //当窗口关闭时，自动保存
         {
             Write();
         }
 
-        //无边框时拖动
-        private void Mouse_MouseMove(object sender, MouseEventArgs e)
+        
+        private void Mouse_MouseMove(object sender, MouseEventArgs e)   //无边框时拖动
         {
             if (e.LeftButton == MouseButtonState.Pressed)
             {
@@ -170,12 +185,12 @@ namespace Arknights_Tool
             }
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private void Button_Click_1(object sender, RoutedEventArgs e)   //关闭窗口按钮
         {
             this.Hide();
         }
 
-        public void Set_StartUpLocation()
+        public void Set_StartUpLocation()   //设置窗口启动初始位置
         {
             var desktopWorkingArea = System.Windows.Forms.Screen.PrimaryScreen.WorkingArea;
             this.Left = desktopWorkingArea.Right - this.Width - 5;
