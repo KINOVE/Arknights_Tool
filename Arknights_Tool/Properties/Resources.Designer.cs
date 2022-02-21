19ed2b9de17433a8e2c686c21f1ac88092bf123a
KINOVE <1437818923@qq.com>
2022-02-21 19:46:33 +0800

    增加了一个小图标

--- Arknights_Tool/Properties/Resources.Designer.cs
+++ Arknights_Tool/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace Arknights_Tool.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   查找 System.Drawing.Bitmap 类型的本地化资源。
+        /// </summary>
+        internal static System.Drawing.Bitmap 理智_1 {
+            get {
+                object obj = ResourceManager.GetObject("理智_1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
